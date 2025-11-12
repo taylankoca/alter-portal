251b7af15b0be266360d9483d2f176b65b8ecb8f
Taylan Koca <taylankoca@gmail.com>
2025-11-12 07:16:53 +0000

    Failed to fetch projects with status: 401. Response: {"message":"Token g

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -6,11 +6,20 @@ export async function POST() {
   try {
     // Remove the authentication cookie
     cookies().set('auth_token', '', {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV !== 'development',
       sameSite: 'strict',
       path: '/',
       expires: new Date(0), // Set expiry date to the past to delete it
+    });
+    
+    // Remove the user cookie
+     cookies().set('user', '', {
+      httpOnly: false,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      path: '/',
+      expires: new Date(0),
     });
     
     return Response.json({ success: true, message: 'Logged out successfully' });
